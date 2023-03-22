af13fdc1f916fc253f20d42b8d89a6a98b2e0bca
Shayan <shayanebrahimi555@yahoo.com>
2023-03-22 21:19:37 +0430

    Learning 1

---
+++ bot.py
@@ -0,0 +1,8 @@
+import telegram.ext 
+
+token = "6004273466:AAF0Cz7nFtrhBJ8HgW5SrnC_k958kNsQatM"
+#print(bot.get_me())
+updater = telgram.ext.Updater("6004273466:AAF0Cz7nFtrhBJ8HgW5SrnC_k958kNsQatM", use_context=True)
+
+
+dispatcher= updater.dispatcher
